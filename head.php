1305fe0b3ab0a7833b6faf58c4b2f157f0b0931b
Gerardo <gerardo.lazuardi@gmail.com>
2019-09-03 10:12:23 +0700

    new

---
+++ head.php
@@ -0,0 +1,7 @@
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+		<title>MobileCom</title>
+		<script src="https://kit.fontawesome.com/b8cf765739.js"></script>
+		<link href="css/index.css" rel="stylesheet">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<?php include 'head.php'; ?>
+	<title></title>
+</head>
+<body>
+	<div class="head-image container-fluid">
+		<div class=" row">
+			<img src="assets/cover/Cover_BG.png" style="position: fixed;width: 100%">
+			<!-- <div class="col-12 d-flex justify-content-center mt-3">
+				<div style="display: inline-block; background-color: #fff;height: 2.5rem;width: 5rem;border-radius: 10rem 10rem 0 0;"></div>
+			</div> -->
+		</div>
+		<div class="row">
+			<div class="col-12">
+				<div class="pb-2 pt-2 text-center" style="color: #000;">
+					<div class="box-wrapper">
+						<div class="circle" style="background-color: #000;"></div>
+						<div class="box-text" style="">
+							<h1 style="font-size: 1rem">Jennifer Lawrence</h1>
+							<h5 style="font-weight: bold; font-size: 1rem">3.687 points</h5>
+						</div>
+					</div>
+				</div>
+				<h5 class="text-center mt-3" style="color: rgba(51,51,51,0.5);">Select Rewards</h5>
+			</div>
+		</div>
+		<div class="row">
+			<div class="main-card col-xl-2 col-lg-3 col-md-3 col-6">
+				<div class="main-card-content">
+					<h5 class="main-card-padding main-card-tittle">Table 3</h5>
+					<div class="main-card-mask">
+						<img class="img-fluid main-card-img" src="img/menu-1.png">
+					</div>
+					<h5 class="main-card-padding main-card-name">Grilled Beef</h5>
+					<h5 class="main-card-padding main-card-qty">2 items</h5>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
