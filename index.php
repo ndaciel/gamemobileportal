cce6e6ee6adbd54fd8e3f0305a4d392101fcd6c8
Gerardo <gerardo.lazuardi@gmail.com>
2019-09-03 11:40:56 +0700

    push3

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	<?php include 'head.php'; ?>
 	<title></title>
 </head>
-<body style="padding-bottom: 1.5rem">
+<body style="padding-bottom: 2rem">
 	<div class="head-image container-fluid">
 		<div class=" row">
 			<img src="assets/cover/Cover_BG.png" style="position: absolute;width: 100%">
@@ -16,96 +16,118 @@
 			<div class="col-12">
 				<div class="pb-2 pt-2 text-center" style="color: #000;">
 					<div class="box-wrapper">
-						<div class="circle" style="background-color: #000;"></div>
+						<div class="circle" style="background-color: #999;"></div>
+						<div class="circle-photo" style="background-color: #FFF;">
+							<img src="assets/jcoph_201603_5.jpg">
+						</div>
 						<div class="box-text" style="">
 							<h1 style="font-size: 1rem">Jennifer Lawrence</h1>
 							<h5 style="font-weight: bold; font-size: 1rem">3.687 points</h5>
 						</div>
 					</div>
 				</div>
-				<h5 class="text-center mt-3" style="color: rgba(51,51,51,0.5);">Select Rewards</h5>
+				<h5 class="text-center mt-3" style="font-size: 1rem; font-family: Roboto;color: rgba(51,51,51,0.5);">Select Rewards</h5>
 			</div>
 		</div>
 		<div class="row">
-			<div class="main-card col-6">
+			<div class="main-card col-6" data-toggle="modal" data-target="#exampleModalCenter">
 				<div class="main-card-content">
 					<div class="main-card-mask">
 						<img class="img-fluid main-card-img" src="assets/jcoph_201603_5.jpg">
 						<div class="overlay-card-color"></div>
-						<h5 class="ml-2" style="font-weight: bold;position: absolute;top:3.5rem;">Donut</h5>
+						<h5 class="reward-card-text ml-2">Donut</h5>
 					</div>
 					<h5 class="main-card-padding main-card-name">100 points</h5>
 				</div>
 			</div>
-			<div class="main-card col-6">
+			<div class="main-card col-6" data-toggle="modal" data-target="#exampleModalCenter">
 				<div class="main-card-content">
 					<div class="main-card-mask">
 						<img class="img-fluid main-card-img" src="assets/jcoph_201603_5.jpg">
 						<div class="overlay-card-color"></div>
-						<h5 class="ml-2" style="font-weight: bold;position: absolute;top:3.5rem;">Donut</h5>
+						<h5 class="reward-card-text ml-2">Donut</h5>
 					</div>
 					<h5 class="main-card-padding main-card-name">100 points</h5>
 				</div>
 			</div>
-			<div class="main-card col-6">
+			<div class="main-card col-6" data-toggle="modal" data-target="#exampleModalCenter">
 				<div class="main-card-content">
 					<div class="main-card-mask">
 						<img class="img-fluid main-card-img" src="assets/jcoph_201603_5.jpg">
 						<div class="overlay-card-color"></div>
-						<h5 class="ml-2" style="font-weight: bold;position: absolute;top:3.5rem;">Donut</h5>
+						<h5 class="reward-card-text ml-2">Donut</h5>
 					</div>
 					<h5 class="main-card-padding main-card-name">100 points</h5>
 				</div>
 			</div>
-			<div class="main-card col-6">
+			<div class="main-card col-6" data-toggle="modal" data-target="#exampleModalCenter">
 				<div class="main-card-content">
 					<div class="main-card-mask">
 						<img class="img-fluid main-card-img" src="assets/jcoph_201603_5.jpg">
 						<div class="overlay-card-color"></div>
-						<h5 class="ml-2" style="font-weight: bold;position: absolute;top:3.5rem;">Donut</h5>
+						<h5 class="reward-card-text ml-2">Donut</h5>
 					</div>
 					<h5 class="main-card-padding main-card-name">100 points</h5>
 				</div>
 			</div>
-			<div class="main-card col-6">
+			<div class="main-card col-6" data-toggle="modal" data-target="#exampleModalCenter">
 				<div class="main-card-content">
 					<div class="main-card-mask">
 						<img class="img-fluid main-card-img" src="assets/jcoph_201603_5.jpg">
 						<div class="overlay-card-color"></div>
-						<h5 class="ml-2" style="font-weight: bold;position: absolute;top:3.5rem;">Donut</h5>
+						<h5 class="reward-card-text ml-2">Donut</h5>
 					</div>
 					<h5 class="main-card-padding main-card-name">100 points</h5>
 				</div>
 			</div>
-			<div class="main-card col-6">
+			<div class="main-card col-6" data-toggle="modal" data-target="#exampleModalCenter">
 				<div class="main-card-content">
 					<div class="main-card-mask">
 						<img class="img-fluid main-card-img" src="assets/jcoph_201603_5.jpg">
 						<div class="overlay-card-color"></div>
-						<h5 class="ml-2" style="font-weight: bold;position: absolute;top:3.5rem;">Donut</h5>
+						<h5 class="reward-card-text ml-2">Donut</h5>
 					</div>
 					<h5 class="main-card-padding main-card-name">100 points</h5>
 				</div>
 			</div>
-			<div class="main-card col-6">
+			<div class="main-card col-6" data-toggle="modal" data-target="#exampleModalCenter">
 				<div class="main-card-content">
 					<div class="main-card-mask">
 						<img class="img-fluid main-card-img" src="assets/jcoph_201603_5.jpg">
 						<div class="overlay-card-color"></div>
-						<h5 class="ml-2" style="font-weight: bold;position: absolute;top:3.5rem;">Donut</h5>
+						<h5 class="reward-card-text ml-2">Donut</h5>
 					</div>
 					<h5 class="main-card-padding main-card-name">100 points</h5>
 				</div>
 			</div>
-			<div class="main-card col-6">
+			<div class="main-card col-6" data-toggle="modal" data-target="#exampleModalCenter">
 				<div class="main-card-content">
 					<div class="main-card-mask">
 						<img class="img-fluid main-card-img" src="assets/jcoph_201603_5.jpg">
 						<div class="overlay-card-color"></div>
-						<h5 class="ml-2" style="font-weight: bold;position: absolute;top:3.5rem;">Donut</h5>
+						<h5 class="reward-card-text ml-2">Donut</h5>
 					</div>
 					<h5 class="main-card-padding main-card-name">100 points</h5>
 				</div>
+			</div>
+			<!-- Modal -->
+			<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+			  <div class="modal-dialog modal-dialog-centered" role="document">
+			    <div class="modal-content" style="border-radius: 0.75rem">
+			      <div class="modal-header" style="border-bottom: 0.175rem solid #e9ecef;padding: 0.5rem 1rem">
+			        <h5 class="modal-title" style="color: rgba(51,51,51,0.5);" id="exampleModalLongTitle">Redeem Rewards</h5>
+			        <button type="button" class="close" data-dismiss="modal" aria-label="Close" style="color: rgba(51,51,51,0.5)">
+			          <span aria-hidden="true">&times;</span>
+			        </button>
+			      </div>
+			      <div class="modal-body">
+			       <h5 class="modal-body-text">Trade <span class="bold">100 points</span> for <span class="bold">Glazzy</span></h5>
+			       <div class="d-flex justify-content-center" style="width: 100%">
+				   		<button type="button" class="btn btn-warning px-5" style="color: #fff">Redeem</button>
+				   </div>
+			      </div>
+			    </div>
+			  </div>
 			</div>
 		</div>
 	</div>
